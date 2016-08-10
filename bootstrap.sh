9558a373a2263893d13a9addc79bded77a50e208
Chapman Siu <chapm0n.siu@gmail.com>
2016-08-10 17:17:14 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,4 +9,3 @@ sudo ln -s /usr/local/bin/pip /usr/bin/
 
 pip install -U numpy scipy scikit-learn
 pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
-pip install git+git://github.com/google/skflow.git
